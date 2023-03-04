bcde20a4ddbebcc7d7a942623860e492b57d811a
Ben Spratling IV <bspratling@mac.com>
2023-03-04 17:51:11 -0600

    Support folders in asset catalogs

--- Sources/SwiftXCAssets/AssetCatalog.swift
+++ Sources/SwiftXCAssets/AssetCatalog.swift
@@ -20,18 +20,41 @@ public struct AssetCatalog {
 		self.path = path
 		let fileManager = FileManager()
 		guard let url = URL(filePath: path)
-			,let subFiles = try? fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
 			else { return nil }
 		
-		assets = subFiles.compactMap({ aUrl in
-			if aUrl.pathExtension == "colorset" {
-				guard let filePath = FilePath(aUrl) else { return nil }
-				return ColorAsset(path: filePath)
-			}
-			//TODO: images
-			//TODO: data
-			return nil
-		})
+		
+		func recursiveAssetFinder(_ folderUrl:URL)->[Asset] {
+			guard let subFiles = try? fileManager.contentsOfDirectory(at: folderUrl, includingPropertiesForKeys: nil) else { return [] }
+			let localColorAssets:[Asset] = subFiles.compactMap({ aUrl in
+				if aUrl.pathExtension == "colorset" {
+					guard let filePath = FilePath(aUrl) else { return nil }
+					return ColorAsset(path: filePath)
+				}
+				//TODO: images
+				//TODO: data
+				return nil
+			})
+			
+			//get sub folders
+			let subFolders = subFiles.filter({
+				return $0.hasDirectoryPath && $0.pathExtension == ""
+			})
+			
+			let subFolderAssets = subFolders.map(recursiveAssetFinder)
+			
+			
+			return localColorAssets + subFolderAssets.reduce([], { $0 + $1 })
+		}
+		
+		assets = recursiveAssetFinder(url)
+		
+		//TODO: support assets inside of folders
+		
+		
+		
+		
+		
+		
 	}
 	
 	//TODO: support assets in folders
